a3b929e413a87b02334a0a55ad836f74dbbe810c
Sean <sdrowan93@gmail.com>
2023-09-11 18:57:52 -0500

    models/ boilerplate code

---
+++ models/Tutor.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Tutor extends Model {}
+
+Tutor.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tutor_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tutor_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  
+  {
+    
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tutor',
+  }
+);
+
+module.exports = Tutor;
