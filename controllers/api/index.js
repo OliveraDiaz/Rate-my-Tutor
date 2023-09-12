5faaec01bf367d6784039de5e49524a681a10864
Palafos23 <josephpalafos24@gmail.com>
2023-09-11 20:56:55 -0500

    index.js added for api folder and tutor routes inputed into tutorRoutes file

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const tutorRoutes = require('./tutorRoutes');
+
+
+router.use('/users', userRoutes);
+router.use('/tutors', tutorRoutes);
+
+
+module.exports = router;
---
+++ controllers/api/tutorRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Tutor, User } = require ('../../models');
+
+
+//all tutors
+router.get('/', async (req, res) => {
+    const tutorData = await Tutor.findAll().catch((err) => { 
+        res.json(err);
+      });
+        const tutors = tutorData.map((tutor) => tutor.get({ plain: true }));
+        res.render('all', { tutors });
+      });
+
+//individual tutors 
+router.get('/:tutor_name', async (req,res) => {
+    try{ 
+        const tutorData = await Tutor.findByPk(req.params.tutor_name);
+        if(!tutorData) {
+            res.status(404).json({message: 'No tutor by this name!'});
+            return;
+        }
+        const tutor = tutorData.get({ plain: true });
+        res.render('tutor', tutor);
+      } catch (err) {
+          res.status(500).json(err);
+      };     
+})
+
+module.exports = router;
+
+
